0812d4902fd27838bbea8cacf4c22da9bed17b1e
sudo200 <65343982+sudo200@users.noreply.github.com>
2021-05-29 19:39:24 +0200

    update text api

--- src/main/java/io/github/bennyboy1695/betterdiscordbridge/listeners/DiscordListener.java
+++ src/main/java/io/github/bennyboy1695/betterdiscordbridge/listeners/DiscordListener.java
@@ -8,12 +8,18 @@ import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.kyori.adventure.text.ComponentLike;
+import net.kyori.adventure.identity.Identity;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
-import org.checkerframework.checker.nullness.qual.NonNull;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.StyleBuilderApplicable;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.function.Consumer;
 
 //h//
 public class DiscordListener extends ListenerAdapter {
@@ -31,7 +37,7 @@ public class DiscordListener extends ListenerAdapter {
     @Override
     public void onReady(@NotNull ReadyEvent event) {
         super.onReady(event);
-        event.getJDA().getGuilds().forEach(Guild::loadMembers);
+        event.getJDA().getGuilds().forEach(Guild :: loadMembers);
     }
 
     @Override
@@ -44,28 +50,29 @@ public class DiscordListener extends ListenerAdapter {
         Guild guild = event.getGuild();
         Member member = event.getMessage().getMember();
 
-        String name = member == null ? author.getName():member.getEffectiveName();
+        if(bridge.getJDA().getSelfUser().getIdLong() == author.getIdLong())
+            return;
+
+        String name = member == null ? author.getName() : member.getEffectiveName();
 
         String msgformat = bridge.getConfig().getFormats("discord_from")
                 .replace("<User>", name)
                 .replace("<Message>", msg);
 
-        @NonNull TextComponent finalmessage = TextComponent.builder(msgformat).build();
-        ComponentLike compfinalmessage = finalmessage.asComponent();
+        Component componentMsg = LegacyComponentSerializer.legacyAmpersand().deserialize(msgformat);
 
         if (channel.getIdLong() == bridge.getConfig().getChannels("global")) {
-            bridge.getProxyServer().sendMessage(compfinalmessage);
-            logger.info("[Discord] " + name + " : " + msg);
+            bridge.getProxyServer().sendMessage(componentMsg);
         } else {
             for (RegisteredServer server : bridge.getProxyServer().getAllServers()) {
                 if (channel.getIdLong() == bridge.getConfig().getChannels(server.getServerInfo().getName())) {
                     for (Player player : server.getPlayersConnected()) {
-                        player.sendMessage(compfinalmessage);
+                        player.sendMessage(Identity.nil(), componentMsg);
                     }
-                    logger.info("[Discord] " + name + " : " + msg);
                 }
             }
         }
+        logger.info(componentMsg.toString());
     }
 }
 
